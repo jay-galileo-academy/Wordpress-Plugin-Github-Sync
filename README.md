1ace194505495e0926083deba70911ea1f6baea4
jay-galileo-academy <jay@galileo-academy.nl>
2024-05-28 10:27:47 +0200

    Update with Github Sync Class

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a test
+
+~Current Version:1.4~
--- github-plugin-sync.php
+++ github-plugin-sync.php
@@ -21,15 +21,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 class GithubPluginSync
 {
 
-    function events() {
-       
-    }
-
     function register() {
-
+        add_action('init', [$this, 'update']);
     }
 
-    function enqueue() {
+    function update() {
+
+        include_once 'handlers/gps-updater.php';
+
+        define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+        if ( is_admin() ) {
+
+            $config = array(
+                'slug' => plugin_basename( __FILE__ ),
+                'proper_folder_name' => 'github-updater',
+                'api_url' => 'https://api.github.com/repos/jay-galileo-academy/Wordpress-Plugin-Github-Sync',
+                'raw_url' => 'https://raw.github.com/jay-galileo-academy/Wordpress-Plugin-Github-Sync/master',
+                'github_url' => 'https://github.com/jay-galileo-academy/Wordpress-Plugin-Github-Sync',
+                'zip_url' => 'https://github.com/jay-galileo-academy/Wordpress-Plugin-Github-Sync/archive/master.zip',
+                'sslverify' => true,
+                'requires' => '3.0',
+                'tested' => '3.3',
+                'readme' => 'README.md',
+                'access_token' => '',
+            );
+
+            new WP_GitHub_Updater( $config );
+
+        }
 
     }
 
@@ -37,7 +57,6 @@ class GithubPluginSync
 
 if ( class_exists( 'GithubPluginSync' ) ) {
     $githubpluginsync = new GithubPluginSync();
-    $githubpluginsync->events();
     $githubpluginsync->register();
 }
 
