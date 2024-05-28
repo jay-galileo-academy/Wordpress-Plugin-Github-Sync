f4704c5931a8e8c801616d24e2b0048bd546fd40
jay-galileo-academy <jay@galileo-academy.nl>
2024-05-28 11:29:01 +0200

    Update

--- github-plugin-sync.php
+++ github-plugin-sync.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Github Plugin Sync
  * Plugin URI:        https://github.com/jay-galileo-academy/Wordpress-Plugin-Github-Sync
  * Description:       Syncs your WordPress plugin with your Github repository.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Jay Schmidt
